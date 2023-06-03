86565727095aa3f7f63678069436fc224604cb21
Harshal Kumre <kumreharshalkumar@gmail.com>
2023-06-04 00:03:37 +0530

    model evalution step added.

--- src/CnnClassifier/config/configuration.py
+++ src/CnnClassifier/config/configuration.py
@@ -1,6 +1,6 @@
 from CnnClassifier.constants import *
 from CnnClassifier.utils.main_utils import read_yaml, create_directories
-from CnnClassifier.entity.config_entity import DataIngestionConfig, PrepareBaseModelConfig, PrepareCallbacksConfig, TrainingConfig
+from CnnClassifier.entity.config_entity import DataIngestionConfig, PrepareBaseModelConfig, PrepareCallbacksConfig, TrainingConfig, EvaluationConfig
 import os
 
 
@@ -90,3 +90,14 @@ class ConfigurationManager:
         )
 
         return training_config
+
+    def get_evaluation_config(self) -> EvaluationConfig:
+        eval_config = EvaluationConfig(
+            path_of_model=Path("artifacts/training/model.h5"),
+            training_data=Path("artifacts/data_ingestion/Chicken-fecal-images"),
+            all_params=self.params,
+            params_image_size=self.params.IMAGE_SIZE,
+            params_batch_size=self.params.BATCH_SIZE
+        )
+
+        return eval_config
