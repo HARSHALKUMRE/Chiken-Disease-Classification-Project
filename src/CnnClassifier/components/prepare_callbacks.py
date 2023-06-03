f912687ff324b50f808aa928354da4cf47a3c199
Harshal Kumre <kumreharshalkumar@gmail.com>
2023-06-03 14:56:07 +0530

    training step added.

--- src/CnnClassifier/components/prepare_callbacks.py
+++ src/CnnClassifier/components/prepare_callbacks.py
@@ -1,26 +1,27 @@
 import os
-import sys
 import urllib.request as request
 from zipfile import ZipFile
-import tensorflow as tf 
+import tensorflow as tf
 import time
 from CnnClassifier.entity.config_entity import PrepareCallbacksConfig
 
+
 class PrepareCallback:
     def __init__(self, config: PrepareCallbacksConfig):
-            self.config = config
+        self.config = config
 
-
-    @property
-    def _create_tb_callbacks(self):
-        timestamp = time.strftime("%Y_%m_%d-%H_%M_%S")
-        tb_running_log_dir = os.path.join(
-            self.config.tensorboard_root_log_dir,
-            f"tb_logs_at_{timestamp}"
-        )
-        return tf.keras.callbacks.Tensorboard(log_dir=tb_running_log_dir)
 
     
+    @property
+    def _create_tb_callbacks(self):
+        timestamp = time.strftime("%Y-%m-%d-%H-%M-%S")
+        tb_running_log_dir = os.path.join(
+            self.config.tensorboard_root_log_dir,
+            f"tb_logs_at_{timestamp}",
+        )
+        return tf.keras.callbacks.TensorBoard(log_dir=tb_running_log_dir)
+    
+
     @property
     def _create_ckpt_callbacks(self):
         return tf.keras.callbacks.ModelCheckpoint(
